5f647fb78568be993642cea773d64b5256fedf96
Ivan Chukhalo <ivanchukhalo@gmail.com>
2023-05-25 19:58:41 +0300

    style: add styles for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
+import './Header.css'
 
 export default function Header(){
     return (
-        <header>
-            <nav>
-                <Link to="/"><DeliveryDiningIcon /> Shop</Link>
-                <Link to="/cart">Cart</Link>
+        <header className='header'>
+            <nav className='nav'>
+                <Link to="/" className="nav__link">
+                    <DeliveryDiningIcon className='test' sx={{fontSize: 48}}/>
+                    Shop
+                </Link>
+                <Link to="/cart" className="nav__link">Cart</Link>
             </nav>
         </header>
     )
