1406118ea633587a98fd6a35168a28214f317669
Ivan Chukhalo <ivanchukhalo@gmail.com>
2023-05-27 00:28:32 +0300

    style: add style for the list of dishes on home page

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './MealCard.css'
 
 export default function MealCard({name, shop, price, description}){
     return(
         <div className='mealCard'>
             <h3 className="mealCard__header">{name}</h3>
-            <p className='mealCard__info'>{price}</p>
-            <p className='mealCard__'>{shop}</p>
+            <p className='mealCard__price mealCard__info'>{price} UAH</p>
+            <p className='mealCard__shop mealCard__info'>{shop}</p>
             <p className='mealCard__description'>{description}</p>
         </div>
     )
